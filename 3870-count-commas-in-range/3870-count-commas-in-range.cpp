ba1f028a59e972d52981fb042dbf4ab5e0f7d4a5
TANISH SHRIMALI <tanishshrimali2023@gmail.com>
2026-09-08 23:32:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countCommas(int n) {
+
+        if(n<1000){
+            return 0;
+        }
+
+        int maxi = 100000;
+
+        int totalNum = n - 1000 + 1;
+
+        return totalNum;
+
+        
+    }
+};
